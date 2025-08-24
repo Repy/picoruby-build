390face832641bf9ad850a5c83067c577a9bedb6
Repy <gamecube02+git@gmail.com>
2025-08-24 12:40:04 +0000

    picoruby最新版に対応

--- picoruby-build.sh
+++ picoruby-build.sh
@@ -4,24 +4,25 @@ wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | 
 sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
 sudo apt install cmake-data
 
+export PICO_SDK_PATH=$(pwd)/pico-sdk
+export PICO_EXTRAS_PATH=$(pwd)/pico-extras
+export PATH=$(pwd)/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin:$PATH
 
 git clone https://github.com/raspberrypi/pico-sdk.git -b 2.1.1
 git clone https://github.com/raspberrypi/pico-extras.git -b sdk-2.1.1
 (cd pico-sdk; git submodule update --init)
-export PICO_SDK_PATH=$(pwd)/pico-sdk
-export PICO_EXTRAS_PATH=$(pwd)/pico-extras
 
 wget https://developer.arm.com/-/media/Files/downloads/gnu/14.2.rel1/binrel/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi.tar.xz
 tar xf arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi.tar.xz 
 
-export PATH=$(pwd)/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin:$PATH
 
 git clone https://github.com/picoruby/R2P2.git -b 0.4.1
+sed -i 's/git\@github\.com\:/https:\/\/github.com\//g' R2P2/.gitmodules
 (cd R2P2; git submodule update --init)
+sed -i 's/git\@github\.com\:/https:\/\/github.com\//g' R2P2/lib/picoruby/.gitmodules
 (cd R2P2/lib/picoruby; git submodule update --init)
+(cd R2P2/lib/picoruby/mrbgems/mruby-compiler2; git submodule update --init)
 
-(cd R2P2; rake setup; rake pico_w)
+(cd R2P2; rake setup; rake mrubyc:pico_w:production)
 
 (cd R2P2; rake clean)
-
-
